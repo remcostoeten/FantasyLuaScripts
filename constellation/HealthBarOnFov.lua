119ff2fb12f4dc01b58a5c0fb0b646affa46026f
Remco Stoetn <remco.stoeten@distil.nl>
2022-12-01 20:51:26 +0100

    Add distance checker.

--- constellation/HealthBarOnFov.lua
+++ constellation/HealthBarOnFov.lua
@@ -1,5 +1,8 @@
 local HealthBarOnFov = {
-    m_iHealth = nil
+    m_iHealth = nil,
+    fov = 15,
+
+    distance = 150
 }
 
 function HealthBarOnFov.Initialize()
@@ -18,6 +21,8 @@ function HealthBarOnFov.OnConstellationTick(localplayer, localweapon, viewangles
     if not player_information then return end
 
     for _, player in pairs(constellation.game.get_enemies()) do
+        local in_fov = constellation.game.is_in_fov
+        local dimensions = constellation.game.get_box_dimensions(player)
         local health = constellation.memory.read_integer(player["address"] + HealthBarOnFov.m_iHealth)
         local ScreenSize = health * 34
         local enemy = constellation.game.get_enemies()
@@ -28,6 +33,12 @@ function HealthBarOnFov.OnConstellationTick(localplayer, localweapon, viewangles
         )
 
         if constellation.game.is_in_fov(player, 8) and result then
+            if dimensions ~= nil then
+                constellation.windows.overlay.box_filled_outlined(dimensions["left"], dimensions["top"],
+                    dimensions["right"],
+                    dimensions["bottom"] + 1, 2, { r = 0, g = 0, b = 0, a = 0 },
+                    { r = 255, g = 255, b = 255, a = 144 })
+            end
             if health > 75 then
                 constellation.windows.overlay.box_filled(0, 0, ScreenSize, 20,
                     { r = 0, g = 255, b = 0, a = 150 })
@@ -43,6 +54,12 @@ function HealthBarOnFov.OnConstellationTick(localplayer, localweapon, viewangles
         end
 
         if constellation.game.is_in_fov(player, 15) and not result then
+            if dimensions ~= nil then
+                constellation.windows.overlay.box_filled_outlined(dimensions["left"], dimensions["top"],
+                    dimensions["right"],
+                    dimensions["bottom"] + 1, 2, { r = 255, g = 0, b = 0, a = 15 },
+                    { r = 255, g = 0, b = 0, a = 15 })
+            end
             if health > 75 then
                 constellation.windows.overlay.box_filled(0, 0, ScreenSize, 20,
                     { r = 0, g = 255, b = 0, a = 255 })
@@ -53,9 +70,38 @@ function HealthBarOnFov.OnConstellationTick(localplayer, localweapon, viewangles
             end
             if health < 40 then
                 constellation.windows.overlay.box_filled(0, 0, ScreenSize, 20,
-                    { r = 255, g = 0, b = 0, a = 255 })
+                    { r = 9, g = 0, b = 255, a = 255 })
             end
         end
+
+        local is_close, direction = constellation.game.is_in_fov(player, HealthBarOnFov.fov)
+
+        if is_close and constellation.game.is_in_fov(player, 3) then
+            constellation.log('is close and in fov 3 9')
+            local distance = math.sqrt(
+                math.pow(
+                    (player.origin.x - player_information.origin.x),
+                    2
+                )
+
+                +
+
+                math.pow(
+                    (player.origin.y - player_information.origin.y),
+                    2
+                )
+            )
+
+            -- -- check if we're close enough.
+            -- if distance <= HealthBarOnFov.distance then
+            --     constellation.log('3')
+            --     -- shoot using kernel mouse simulation
+            --     constellation.driver.click()
+
+            -- end
+
+        end
+
     end
 end
 
